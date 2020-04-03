8de14df7402bf2247e4ef66e6234a0a605dfd09d
xuwakao <xuwakao@gmail.com>
2020-04-04 00:28:48 +0800

    fix activity task description and title;

--- VirtualApp/lib/src/main/java/com/lody/virtual/client/hook/plugin/PluginFixer.java
+++ VirtualApp/lib/src/main/java/com/lody/virtual/client/hook/plugin/PluginFixer.java
@@ -6,6 +6,7 @@ import android.content.pm.ApplicationInfo;
 import android.content.res.Resources;
 
 import com.lody.virtual.client.core.VirtualCore;
+import com.lody.virtual.client.fixer.ActivityFixer;
 import com.lody.virtual.server.pm.PackageSetting;
 
 import mirror.android.content.ContextWrapper;
@@ -31,9 +32,10 @@ public class PluginFixer {
      * @param plugin
      */
     public static void fixActivity(Activity activity, PluginImpl plugin) {
-        int icon = plugin.getApplicationInfo().icon;
-        int logo = plugin.getApplicationInfo().logo;
-        int themeResId = plugin.getApplicationInfo().theme;
+        ApplicationInfo pluginAppInfo = plugin.getApplicationInfo();
+        int icon = pluginAppInfo.icon;
+        int logo = pluginAppInfo.logo;
+        int themeResId = pluginAppInfo.theme;
         activity.setTheme(themeResId);
         ApplicationInfo info = ComponentInfo.applicationInfo.get(mirror.android.app.Activity.mActivityInfo.get(activity));
         if (icon > 0) {
@@ -54,12 +56,14 @@ public class PluginFixer {
 
         mirror.android.app.Activity.mApplication.set(activity, plugin.getApp());
         ContextThemeWrapper.mResources.set(activity, plugin.getPluginContext().getResources());
-        if (plugin.getApplicationInfo().theme > 0) {
+        if (pluginAppInfo.theme > 0) {
             Resources.Theme theme = plugin.getPluginContext().getResources().newTheme();
-            theme.applyStyle(plugin.getApplicationInfo().theme, true);
+            theme.applyStyle(pluginAppInfo.theme, true);
             ContextThemeWrapper.mTheme.set(activity, theme);
         }
         PhoneWindow.mLayoutInflater.set(activity.getWindow(), plugin.getPluginContext().getSystemService(LAYOUT_INFLATER_SERVICE));
         ContextWrapper.mBase.set(activity, plugin.getPluginContext());
+        activity.setTitle(plugin.getPluginContext().getResources().getString(pluginAppInfo.labelRes));
+        ActivityFixer.fixActivity(activity);
     }
 }
